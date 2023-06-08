ae7196f5963b4de1f379ad8e34f50c692800806e
Ana Claudia <a.claudia@estudante.ifro.edu.br>
2023-06-08 14:24:20 -0400

    Commit index
    Commit index

--- src/api/components/index.ts
+++ src/api/components/index.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Router } from 'express'; 
 import { UserRoutes } from './user/user.routes';
 import { BaseRoutes } from './base/base.routes';
 import { ConsultaRoutes } from './consulta/consulta.routes';
@@ -8,7 +8,7 @@ import { RecebimentoRoutes } from './recebimento/recebimento.routes';
  * Init component routes
  *
  * @param {Router} router
- * @param {string} prefix
+ * @param {string} prefix 
  * @returns {void}
  */
 export function registerRoutes(router: Router, prefix: string = ''): void {
@@ -17,4 +17,4 @@ export function registerRoutes(router: Router, prefix: string = ''): void {
   router.use(`${prefix}/consulta`, new ConsultaRoutes().routes());
   router.use(`${prefix}/paciente`, new PacienteRoutes().routes());
   router.use(`${prefix}/recebimento`, new RecebimentoRoutes().routes());
-}
+} 
